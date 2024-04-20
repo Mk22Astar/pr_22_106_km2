377d60864dbb492c559ce6fd728ca622b01e56fc
bot <londanv@yandex.ru>
2024-04-20 12:06:48 +0700

    м1

---
+++ app/src/main/java/com/example/pr_22_106_km/Abonementi.java
@@ -0,0 +1,14 @@
+package com.example.pr_22_106_km;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Abonementi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.abonementi);
+    }
+}
