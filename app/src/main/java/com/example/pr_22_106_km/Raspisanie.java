bded26cac33994ebc41332628a7fee51529671b4
bot <londanv@yandex.ru>
2024-04-26 23:35:04 +0700

    версия 2

--- app/src/main/java/com/example/pr_22_106_km/Raspisanie.java
+++ app/src/main/java/com/example/pr_22_106_km/Raspisanie.java
@@ -2,7 +2,9 @@ package com.example.pr_22_106_km;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Raspisanie extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class Raspisanie extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.raspisanie);
+
     }
+
+    public void onClick(View view)
+    {
+        Intent intent = new Intent(this, Prepodavateli.class);
+        startActivity(intent);
+
+
+    }
+
+
 }
